9d5e7a3abaac6d49129210d8fd46fa66577085b2
Maia Bard <Maiakbard@gmail.com>
2025-02-19 09:29:54 -0700

    Add passenger tests/code to file

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -7,5 +7,34 @@ end
 
 # Your tests should start here!
 RSpec.describe Passenger do 
-    
+  before(:each) do
+    @passenger = Passenger.new({})
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  end
+
+  it 'exists' do
+    expect(@passenger).to be_a(Passenger)
+  end
+
+  it 'has a name' do
+    expect(@charlie.name).to eq("Charlie")
+  end
+
+  it 'has an age' do
+    expect(@charlie.age).to eq(18)
+  end
+
+  it 'can determine if passanger is an adult' do
+    expect(@charlie.adult?).to eq(true)
+    expect(@taylor.adult?).to eq(false)
+  end
+
+  it 'can determine if passenger is a driver' do
+    expect(@charlie.driver?).to eq(false)
+
+    @charlie.drive
+
+    expect(@charlie.driver?).to eq(true)
+  end
 end
