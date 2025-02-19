846b6bf582a7f626bc39705459c2c056184fa8f8
Maia Bard <Maiakbard@gmail.com>
2025-02-19 10:29:52 -0700

    Add initialized intansces year, make, model

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,6 +2,9 @@ class Vehicle
     attr_reader :year, :make, :model, :passengers
 
     def initialize(year, make, model)
+        @year = year
+        @make = make
+        @model = model
         @speeding = false
         @passengers = []
     end
