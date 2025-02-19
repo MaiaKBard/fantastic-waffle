01de8545ece96f94bf0e60942012df1b4d15f131
Maia Bard <Maiakbard@gmail.com>
2025-02-19 09:36:29 -0700

    Create Vehicle class aand initialize method

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,16 @@
+require './lib/vehicle'
+require './lib/passenger'
+
+RSpec.describe Vehicle do 
+    before(:each) do
+        @vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        @jude = Passenger.new({"name" => "Jude", "age" => 20})
+        @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    end
+
+    it 'exists' do
+        expect(@vehicle).to be_a(Vehicle)
+    end
+end
