d892591ce78b722cd6e60841d077506b0317f25e
Maia Bard <Maiakbard@gmail.com>
2025-02-19 10:25:31 -0700

    Add methods sppeding?, add_passanger(), num_adults to test/code files

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,6 +1,26 @@
 class Vehicle
-    attr_reader :year, :make, :model
+    attr_reader :year, :make, :model, :passengers
 
     def initialize(year, make, model)
+        @speeding = false
+        @passengers = []
+    end
+
+    def speeding?
+        @speeding
+    end
+
+    def speed
+        @speeding = true
+    end
+
+    def add_passenger(passenger)
+        @passengers << passenger
+    end
+
+    def num_adults
+        @passengers.count do |passenger|
+            passenger.age >= 18
+        end
     end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,5 +1,6 @@
 require './lib/vehicle'
 require './lib/passenger'
+require 'pry'
 
 RSpec.describe Vehicle do 
     before(:each) do
@@ -12,5 +13,38 @@ RSpec.describe Vehicle do
 
     it 'exists' do
         expect(@vehicle).to be_a(Vehicle)
+    end
+
+    it 'can check if vehicle is speeding' do
+        expect(@vehicle.speeding?).to eq(false)
+
+        @vehicle.speed
+
+        expect(@vehicle.speeding?).to eq(true)
+    end
+
+    it 'can have passengers' do
+        expect(@vehicle.passengers).to eq([])
+    end
+
+    it 'can add passengers' do
+        expect(@vehicle.passengers).to eq([])
+
+        @vehicle.add_passenger(@charlie)
+        @vehicle.add_passenger(@jude)
+        @vehicle.add_passenger(@taylor)
+
+        expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
+    end
+
+    it 'can count the number of adults in the vehicle' do
+        expect(@vehicle.passengers).to eq([])
+
+        @vehicle.add_passenger(@charlie)
+        @vehicle.add_passenger(@jude)
+        @vehicle.add_passenger(@taylor)
+
+        expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
+        expect(@vehicle.num_adults).to eq(2)
     end
 end
